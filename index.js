577458d220a9587b9437f1271c3aaef36f240060
Tobías Nahuel Mercuri <mercuritobias16@gmail.com>
2025-06-18 15:53:25 -0300

    Prueba del catalogo

---
+++ index.js
@@ -0,0 +1,46 @@
+let button = document.getElementById("myButton");
+let isBlack = false;
+
+button.addEventListener("click", function() {
+  if (isBlack === true) {
+    document.body.style.background = '#ccc';
+    document.body.style.color = "black";
+    button.innerHTML = '<i class="bi bi-moon-fill"></i>';
+    button.classList.remove('btn-dark');
+    button.classList.add('btn-light');
+
+    document.querySelectorAll('.card').forEach(card => {
+      card.style.backgroundColor = 'white';
+      card.style.color = 'black';
+    });
+
+    document.querySelector('header').style.backgroundColor = '#007bff';
+    document.querySelector('footer').style.backgroundColor = '#007bff';
+    document.querySelector('footer').style.color = 'white';
+
+    document.querySelector('.navbar').classList.remove('bg-dark');
+    document.querySelector('.navbar').classList.add('bg-primary');
+
+    isBlack = false;
+  } else {
+    document.body.style.background = 'black';
+    document.body.style.color = '#ccc';
+    button.innerHTML = '<i class="bi bi-sun-fill"></i>';
+    button.classList.remove('btn-light');
+    button.classList.add('btn-dark');
+
+    document.querySelectorAll('.card').forEach(card => {
+      card.style.backgroundColor = '#1e1e1e';
+      card.style.color = '#ccc';
+    });
+
+    document.querySelector('header').style.backgroundColor = '#222';
+    document.querySelector('footer').style.backgroundColor = '#222';
+    document.querySelector('footer').style.color = '#ccc';
+
+    document.querySelector('.navbar').classList.remove('bg-primary');
+    document.querySelector('.navbar').classList.add('bg-dark');
+
+    isBlack = true;
+  }
+});
