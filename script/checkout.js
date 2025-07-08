34847ca02de11c58aaf69cdb7ecde837bdba9873
tobyy-m <mercuritobias16@gmail.com>
2025-07-08 00:57:39 -0300

    solucion al envio de mail (espero)

--- script/checkout.js
+++ script/checkout.js
@@ -249,7 +249,7 @@ document.addEventListener("DOMContentLoaded", function() {
 async function enviarEmailClienteSeguro(formData) {
     try {
         const datos = Object.fromEntries(formData);
-        const response = await fetch(`https://formsubmit.co/ajax/${datos.email}`, {
+        const response = await fetch(`https://formsubmit.co/ajax/5258143f5384bf121bb077a3094b9e58`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
